f3117851217a7751b46f5803caeed642aa2faed8
luizjunior-ciandt <luiz.junior@ciandt.com>
2023-01-13 11:27:02 -0300

    feature: finishing pokemon list screen feature: creating view model

--- app/src/main/java/com/example/pokedexcompose/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/pokedexcompose/ui/navigation/Screen.kt
@@ -2,6 +2,6 @@ package com.example.pokedexcompose.ui.navigation
 
 sealed class Screen(val route: String) {
     object PokemonListScreen: Screen("pokemon_list_screen")
-    object PokemonDetailsScreen: Screen("pokemon_details_screen/{dominantColor}/{pokemonName}")
+    object PokemonDetailsScreen: Screen("pokemon_details_screen")
 
 }
---
+++ app/src/main/java/com/example/pokedexcompose/ui/screen/PokemonListViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.pokedexcompose.ui.screen
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+import androidx.palette.graphics.Palette
+import com.example.pokedexcompose.repository.PokemonRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PokemonListViewModel @Inject constructor(
+    private val repository: PokemonRepository
+) : ViewModel() {
+
+
+    fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
+        val bitmap = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+        Palette.from(bitmap).generate { palette ->
+            palette?.dominantSwatch?.rgb?.let { colorValue ->
+                onFinish(Color(colorValue))
+            }
+        }
+    }
+}
