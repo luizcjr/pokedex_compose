a98237a3678add351083bff89d46866745826c81
luizjunior-ciandt <luiz.junior@ciandt.com>
2023-01-13 11:00:49 -0300

    feature: creating pokemon list screen

--- app/src/main/java/com/example/pokedexcompose/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/pokedexcompose/ui/navigation/NavGraph.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.pokedexcompose.ui.screen.PokemonListScreen
 
 @Composable
 fun SetupNavHost(navController: NavHostController) {
@@ -16,7 +17,7 @@ fun SetupNavHost(navController: NavHostController) {
         startDestination = Screen.PokemonListScreen.route
     ) {
         composable(route = Screen.PokemonListScreen.route) {
-
+            PokemonListScreen(navController)
         }
 
         composable(route = Screen.PokemonDetailsScreen.route,
